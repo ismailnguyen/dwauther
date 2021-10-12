b3c02d71faeee9efa7317aa6df5c6c87358c1059
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-10-12 23:57:57 +0200

    Remove config file

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
         chrome.scripting.executeScript({
             target: { tabId: tabId },
-            files: ["jquery-3.1.1.min.js", "config.js", "otplib_buffer.js", "otplib_index.js", "dwauther.js"]
+            files: ["jquery-3.1.1.min.js", "otplib_buffer.js", "otplib_index.js", "dwauther.js"]
         })
-            .then(() => {
-                console.log("INJECTED THE FOREGROUND SCRIPT.");
-            })
-            .catch(err => console.log(err));
+        .then(() => {
+            console.log("INJECTED THE FOREGROUND SCRIPT.");
+        })
+        .catch(err => console.log(err));
     }
 });
--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-	username: "", // business manager username (e.g. ismail.nguyen@mycompany.com)
-	password: "", // business manager password
-	otpSecret: "" // OTP secret (e.g. 45468FEZFB1ZE8R3ZR4BZ21ZER)
-}
