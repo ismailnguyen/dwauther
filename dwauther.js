da28f2a14fc70488da54126bd1b71676490cf697
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-10-12 23:59:08 +0200

    Use chrome storage api to store credentials

--- dwauther.js
+++ dwauther.js
@@ -2,46 +2,49 @@ var usernameFilled = false
 var passwordFilled = false
 var mfaFilled = false
 
-var auth = function () {
+var auth = function (username, password) {
   if (!usernameFilled && $('#idToken1').length) {
-    $('#idToken1').val(config.username)
+    $('#idToken1').val(username)
     $('#loginButton_0').click()
     usernameFilled = true
   }
 
   if (!usernameFilled) {
-    setTimeout(auth, 500)
+    setTimeout(function () { auth(username, password) }, 500)
     return
   }
 
-  console.log('try fill password')
   if (!passwordFilled && $('#idToken2').length) {
-    $('#idToken2').val(config.password)
+    $('#idToken2').val(password)
     $('#loginButton_0').click()
     passwordFilled = true
-    console.log('filled pwd with', config.password)
   }
 
   if (!passwordFilled)
-    setTimeout(auth, 500)
+    setTimeout(function () { auth(username, password) }, 500)
 }
 
-var verify = function () {
+var verify = function (otp_secret) {
   if (!mfaFilled && $('input[type=text]').length) {
-    var mfaKey = window.otplib.authenticator.generate(config.otpSecret)
+    var mfaKey = window.otplib.authenticator.generate(otp_secret)
     $('input[type=text]').val(mfaKey)
     $('button[type=submit]').click()
     mfaFilled = true
   }
 
   if (!mfaFilled)
-    setTimeout(verify, 500)
+    setTimeout(function () { verify(otp_secret) }, 500)
 }
 
 $(document).ready(function() {
-  if (window.location.href.indexOf('account.demandware.com/dwsso') > -1)
-    auth()
+  chrome.storage.local.get(['dwauther_config'], function(result) {
+    var conf = result.dwauther_config
+    if (conf) {
+      if (window.location.href.indexOf('account.demandware.com/dwsso') > -1)
+        auth(conf.email, conf.password)
 
-  if (window.location.href.indexOf('verify.salesforce.com') > -1)
-    verify()
+      if (window.location.href.indexOf('verify.salesforce.com') > -1)
+        verify(conf.otp_secret)
+    }
+  })
 })
