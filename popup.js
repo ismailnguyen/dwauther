e047361614f2fecfdf642989f0761683c4c42dcd
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-10-19 12:54:41 +0200

    Add a tab to generate token and see it

--- popup.js
+++ popup.js
@@ -5,19 +5,41 @@ function save(email, password, otp_secret) {
         otp_secret: otp_secret
     }
 	
-    chrome.storage.local.set( {dwauther_config: config}, () => {
-        console.log('dwauther_config saved successfully') ;
-    }) ;
+    if (chrome && chrome.storage && chrome.storage.local) {
+        chrome.storage.local.set( {dwauther_config: config}, () => {
+            console.log('dwauther_config saved successfully')
+        });
+    }
 }
 
-$(document).ready(function() {
-    chrome.storage.local.get(['dwauther_config'], function(result) {
-        var dwauther_config = result.dwauther_config
-        if (dwauther_config) {
-            $('#email-input').val(dwauther_config.email)
-            $('#password-input').val(dwauther_config.password)
-            $('#otp-secret-input').val(dwauther_config.otp_secret)
-        }
+function generateMfaKey (otp_secret) {
+    return window.otplib.authenticator.generate(otp_secret)
+}
+
+$(document).ready(function () {
+    if (chrome && chrome.storage && chrome.storage.local) {
+        chrome.storage.local.get(['dwauther_config'], function(result) {
+            var dwauther_config = result.dwauther_config
+            if (dwauther_config) {
+                $('#email-input').val(dwauther_config.email)
+                $('#password-input').val(dwauther_config.password)
+                $('#otp-secret-input').val(dwauther_config.otp_secret)
+                $('#token-input').val(generateMfaKey(dwauther_config.otp_secret))
+            }
+        })
+    }
+
+    $('li.tab').click(function () {
+        save($('#email-input').val(), $('#password-input').val(), $('#otp-secret-input').val())
+        var id = $(this).data('tab-id')
+        $('#' + id + '.content-tab').show()
+        $(this).addClass('is-active')
+        $('.content-tab:not(#' + id + ')').hide()
+        $('.tab:not([data-tab-id=' + id + '])').removeClass('is-active')
+    })
+
+    $('#generate-btn').click(function() {
+        window.location.reload()
     })
 
     $('#save-btn').click(function() {
